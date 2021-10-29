b5c967ee9960307cd4ed772789d5cefb3be9a60c
xingyifan <xingyifan10@qq.com>
2021-10-29 21:09:16 +0800

    ajax

--- fupeng/js-web-api/ajax.js
+++ fupeng/js-web-api/ajax.js
@@ -1,40 +1,41 @@
-// const xhr = new XMLHttpRequest()
-// xhr.open('GET', '/data/test.json', true)
-// xhr.onreadystatechange = function () {
-//     if (xhr.readyState === 4) {
-//         if (xhr.status === 200) {
-//             // console.log(
-//             //     JSON.parse(xhr.responseText)
-//             // )
-//             alert(xhr.responseText)
-//         } else if (xhr.status === 404) {
-//             console.log('404 not found')
-//         }
-//     }
-// }
-// xhr.send(null)
-
-function ajax(url) {
-    const p = new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest()
-        xhr.open('GET', url, true)
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    resolve(
-                        JSON.parse(xhr.responseText)
-                    )
-                } else if (xhr.status === 404 || xhr.status === 500) {
-                    reject(new Error('404 not found'))
-                }
-            }
+const xhr = new XMLHttpRequest()
+xhr.open('GET', '/data/test.json', true)   // true// 是异步，false是同步
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+            console.log(
+                JSON.parse(xhr.responseText)
+            )
+            alert(xhr.responseText)
+        } else if (xhr.status === 404) {
+            console.log('404 not found')
         }
-        xhr.send(null)
-    })
-    return p
+    }
 }
+xhr.send(null)  // post请求，此处发送json串
 
-const url = '/data/test.json'
-ajax(url)
-.then(res => console.log(res))
-.catch(err => console.error(err))
+
+// function ajax(url) {
+//     const p = new Promise((resolve, reject) => {
+//         const xhr = new XMLHttpRequest()
+//         xhr.open('GET', url, true)
+//         xhr.onreadystatechange = function () {
+//             if (xhr.readyState === 4) {
+//                 if (xhr.status === 200) {
+//                     resolve(
+//                         JSON.parse(xhr.responseText)
+//                     )
+//                 } else if (xhr.status === 404 || xhr.status === 500) {
+//                     reject(new Error('404 not found'))
+//                 }
+//             }
+//         }
+//         xhr.send(null)
+//     })
+//     return p
+// }
+//
+// const url = '/data/test.json'
+// ajax(url)
+// .then(res => console.log(res))
+// .catch(err => console.error(err))
